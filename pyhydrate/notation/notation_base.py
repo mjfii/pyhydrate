f6031ff0264c96fef964663cf05ba185057ee8cc
Michael Flanigan <7891356+mjfii@users.noreply.github.com>
2024-02-03 21:24:18 -0700

    [UPDATE] first pass at docstrings creation

--- pyhydrate/notation/notation_base.py
+++ pyhydrate/notation/notation_base.py
@@ -1,4 +1,15 @@
+"""
+Provides shared base functionality for Notation classes.
+
+The NotationBase class implements common methods and attributes
+needed by all Notation objects such as formatted output, type
+checking, recursion depth tracking, etc.
+
+Child classes inherit from NotationBase to get this base functionality.
+"""
 from typing import Union
+from typing import Pattern
+from typing import Any
 import json
 import yaml
 import re
@@ -8,50 +19,80 @@ from .notation_representation import NotationRepresentation
 
 class NotationBase(NotationRepresentation):
     """
+    Base Notation class with shared attributes and methods.
 
+    NotationBase provides common functionality needed across all
+    Notation classes like formatted output, casting keys, tracking
+    depth, etc. Child classes will inherit this to reduce code duplication.
+
+    Attributes:
+        _raw_value: The original untouched value.
+        _cleaned_value: The processed value after type checking.
+        _hydrated_value: The wrapped Notation object.
+        _cast_pattern (str): Regex pattern for formatting object/dict keys.
+        _kwargs: Additional options passed during initialization.
+        _depth (int): Recursion depth.
     """
-    # CONSTANTS
+
+    # CLASS CONSTANTS
     _source_key: str = '__SOURCE_KEY__'
     _cleaned_key: str = '__CLEANED_KEY__'
     _hydrated_key: str = '__HYDRATED_KEY__'
+    _cast_pattern: Pattern[Any] = re.compile(r'(?<!\d)(?=\d)|(?<=\d)(?!\d)|(?<=[a-z])(?=[A-Z])')
 
     # CLASS VARIABLES
     _raw_value: Union[dict, list, None] = None
     _cleaned_value: Union[dict, list, None] = None
     _hydrated_value: Union[dict, list, None] = None
     _kwargs: dict = {}
-
-    # r'(?=[A-Z])' - pascal case without number separation
-    # r'(?<!^)(?=[A-Z])' - camel case without number separation
-    # r'(?<!^)(?=[A-Z])|(?<=[a-z])(?=\d)'  # - camel case with number separation
-    # r'(?<!^)(?=[A-Z])|(?<=-)(?=[a-z])|(?<=[a-z])(?=\d)'
-    _cast_pattern = re.compile(r'(?<!\d)(?=\d)|(?<=\d)(?!\d)|(?<=[a-z])(?=[A-Z])')
-    _indent: int = 3
     _depth: int = 1
-    _request: str = 'value'
 
     # EXTERNAL METHODS
     def yaml(self) -> Union[str, None]:
+        """
+        Serialize the value to YAML format.
+
+        Returns:
+            str: The YAML string if value is dict/list, else the `element`
+                 value of the NotationPrimitive.
+        """
         if isinstance(self._value, dict) or isinstance(self._value, list):
             return yaml.dump(self._value, sort_keys=False, Dumper=NotationDumper).rstrip()
         else:
             return yaml.dump(self._element, sort_keys=False, Dumper=NotationDumper).rstrip()
+        # TODO: handle None
 
     def json(self) -> Union[str, None]:
+        """
+        Serialize the value to JSON format.
+
+        Returns:
+            str: The JSON string if value is dict/list, else None.
+        """
         return json.dumps(self._value, indent=self._indent)
+        # TODO: handle None
 
     # INTERNAL METHODS
-    def cast_key(self, string: str) -> Union[str, None]:
+    def _cast_key(self, string: str) -> Union[str, None]:
         """
+        Format keys to be lowercase and underscore separated.
 
-        :param string:
-        :return:
+        Parameters:
+            string (str):
+        Returns:
+            str: asdf
         """
         _kebab_clean: str = string.replace('-', '_').replace(' ', '_')
         _parsed = self._cast_pattern.sub('_', _kebab_clean)
         return re.sub(r'_+', r'_', _parsed).lower().strip('_')
 
     def _print_debug(self, request: str, request_value: Union[str, int], stop: bool = False) -> None:
+        """
+        Print debug info about the object.
+
+        Returns:
+            str:
+        """
 
         _component_type: Union[str, None] = None
         _output: Union[str, None] = None
@@ -74,10 +115,37 @@ class NotationBase(NotationRepresentation):
 
     # MAGIC METHODS
     def __str__(self) -> str:
+        """
+        Print the object in YAML format by default.
+
+        This allows print(my_obj) to show a readable YAML string.
+
+        Returns:
+            str: The object in YAML format.
+        """
         return self.yaml()
 
-    def __call__(self, *args, **kwargs) -> Union[dict, str, int, type, None]:
+    def __call__(self, *args, **kwargs) -> Union[dict, list, str, int, float, bool, type, None]:
+        """
+        Call the object as a function to get specific values.
 
+        Allowed values for args[0] are:
+            - 'value': The cleaned _value attribute.
+            - 'element': The _element attribute.
+            - 'type': The object's type.
+            - 'depth': The recursion depth.
+
+        Utilized key/values for kwargs are:
+            - debug: TODO:
+            - indent: TODO:
+
+        Args:
+            *args: The value to retrieve.
+            **kwargs: Additional options.
+
+        Returns:
+            dict, list, str, int, float, bool, type, None: Requested value, element, structure, or type.
+        """
         #
         _stop: bool = kwargs.get('stop', False)
 
@@ -107,18 +175,43 @@ class NotationBase(NotationRepresentation):
     # INTERNAL READ-ONLY PROPERTIES
     @property
     def _element(self) -> Union[dict, None]:
+        """
+        The dict representation of the structure or primitive. There is one
+        key and one value. The <key> is the type, and <value> is the value.
+
+        Returns
+            dict: {type: structure | primitive}
+        """
         return {self._type.__name__: self._value}
 
     @property
     def _value(self) -> Union[dict, list, None]:
+        """
+        The cleaned value(s), i.e. keys are converted to lower case snake.
+
+        Returns:
+            str: the value with cleaned object keys.
+        """
         return self._cleaned_value
 
     @property
     def _type(self) -> type:
+        """
+        The requested value's (structure or primitive) type.
+
+        Returns:
+            type: dict, list, str, int, float, bool, NoneType
+        """
         return type(self._value)
 
     @property
     def _map(self) -> Union[dict, list, None]:
+        """
+        TBD
+
+        Returns:
+            dict, list: TBD
+        """
         return None
 
 
